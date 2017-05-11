0b096bb619da534ec0afb74de7c14c6ebdc47063
OllieFord <ollie.ford343@gmail.com>
2017-05-11 19:05:01 +0200

    added fullscreen map

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,17 +1,17 @@
-var map;
-    $(document).ready(function(){
-      map = new GMaps({
-        el: '#map',
-        lat: -12.043333,
-        lng: -77.028333,
-        zoomControl : true,
-        zoomControlOpt: {
-            style : 'SMALL',
-            position: 'TOP_LEFT'
-        },
-        panControl : false,
-        streetViewControl : false,
-        mapTypeControl: false,
-        overviewMapControl: false
-      });
-    });
+
+function initMap(){
+  const map = new GMaps({
+    el: '#map',
+    lat: -12.043333,
+    lng: -77.028333,
+    zoomControl : true,
+    zoomControlOpt: {
+        style : 'SMALL',
+        position: 'TOP_LEFT'
+    },
+    panControl : true,
+    streetViewControl : false,
+    mapTypeControl: false,
+    overviewMapControl: false
+  });
+}
