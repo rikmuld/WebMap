7739ce186cfae9cef7f6a3432574ac536b9fd580
OllieFord <ollie.ford343@gmail.com>
2017-05-11 17:08:39 +0200

    Added links to scripts

---
+++ public/scripts/map.js
@@ -0,0 +1,17 @@
+var map;
+    $(document).ready(function(){
+      map = new GMaps({
+        el: '#map',
+        lat: -12.043333,
+        lng: -77.028333,
+        zoomControl : true,
+        zoomControlOpt: {
+            style : 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl : false,
+        streetViewControl : false,
+        mapTypeControl: false,
+        overviewMapControl: false
+      });
+    });
