1be6948b8a22603ae37bfd87850d02f0be5cecff
rikmuld <rikmuld@gmail.com>
2017-05-30 21:31:37 +0200

    got rid of all non constants

---
+++ public/scripts/buttons.ts
@@ -0,0 +1,126 @@
+class SimpleControl {
+    div: HTMLDivElement
+    map: google.maps.Map
+
+    constructor(map: google.maps.Map, position: google.maps.ControlPosition, id: string) {
+        const instance = this
+
+        this.map = map
+
+        this.div = document.createElement('div')
+        this.div.id = id
+
+        this.div.addEventListener<"click">("click", function (this: HTMLDivElement, event: MouseEvent) {
+            instance.click(this, event)
+        })
+
+        webMap.controls[position].push(this.div)
+    }
+
+    click(div: HTMLDivElement, ev: MouseEvent) { }
+}
+
+class AddLocation extends SimpleControl {
+    location: LocationControl
+
+    constructor(map: google.maps.Map, position: google.maps.ControlPosition, id: string, loc: LocationControl) {
+        super(map, position, id)
+        this.location = loc
+    }
+
+    click(div: HTMLDivElement, ev?: MouseEvent): any {
+        navigator.geolocation.getCurrentPosition(pos => placeMarker(this.map, toLatlon(pos)))
+        this.location.act()
+    }
+}
+
+class LocationControl extends SimpleControl {
+    constructor(map: google.maps.Map, position: google.maps.ControlPosition, id: string) {
+        super(map, position, id)
+
+        const img = document.createElement('img')
+        img.src = "http://maps.gstatic.com/tactile/mylocation/mylocation-sprite-2x.png"
+
+        this.div.appendChild(img)
+    }
+
+    act() {
+        if (navigator.geolocation) {
+            this.div.firstElementChild.classList.remove("error")
+            navigator.geolocation.getCurrentPosition(position => webMap.setCenter(toLatlon(position)))
+        } else {
+            this.div.firstElementChild.classList.add("error")
+        }
+    }
+
+    click(div: HTMLDivElement, ev?: MouseEvent): any {
+        this.act()
+    }
+
+    error(error: boolean) {
+        if (error) this.div.firstElementChild.classList.add("error")
+        else this.div.firstElementChild.classList.remove("error")
+    }
+}
+
+class SearchBar extends SimpleControl {
+    search: google.maps.places.SearchBox
+    markers: google.maps.Marker[] = []
+
+    constructor(map: google.maps.Map, position: google.maps.ControlPosition, id: string) {
+        super(map, position, id)
+
+        const input = document.createElement('input')
+        input.placeholder = "Search for places, markers, users, tags..."
+
+        this.div.appendChild(input)
+        this.search = new google.maps.places.SearchBox(input)
+
+        const instance: SearchBar = this
+
+        map.addListener('bounds_changed', () => instance.search.setBounds(map.getBounds()))
+        this.search.addListener('places_changed', () => instance.searchChanged())
+    }
+
+    searchChanged() {
+        const instance = this
+        const places = this.search.getPlaces();
+
+        this.markers.forEach(marker => marker.setMap(null))
+        this.markers = []
+
+
+        if (places.length == 0) {
+            return;
+        }
+
+        const bounds = new google.maps.LatLngBounds();
+
+        places.forEach(place => {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return
+            }
+
+            const icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            }
+
+            this.markers.push(new google.maps.Marker({
+                map: this.map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }))
+
+            if (place.geometry.viewport) bounds.union(place.geometry.viewport)
+            else bounds.extend(place.geometry.location)
+        })
+
+        this.map.fitBounds(bounds)
+    }
+}
